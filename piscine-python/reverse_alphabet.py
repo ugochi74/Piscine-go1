7ed6635aaca1479eac8af3e87072a3a405c5c861
ugochi74 <confidenceodo4@gmail.com>
2026-07-16 11:23:04 +0100

    learning

---
+++ piscine-python/reverse_alphabet.py
@@ -0,0 +1,7 @@
+import string
+
+reversed_alphabet = string.ascii_letters[::-1]
+print(reversed_alphabet)
+print(string.ascii_uppercase)
+print(string.ascii_lowercase)
+print(string.ascii_letters)
---
+++ piscine-python/reverse_digit.py
@@ -0,0 +1,8 @@
+import string
+
+
+reversed_number = string.digits[::-1]
+reversed_number = int(reversed_number)
+print(reversed_number)
+#reversed_number = int(string.digits)
+#print(reversed_number)
